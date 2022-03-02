5858749091d3884dd4022acfeb0a086738f92c49
maq7 <maxpet16@mail.ru>
2022-03-02 23:54:46 +0500

    Have been first described DeviceController; add npm: uuid, express-fileupload

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const express = require('express');
 const sequelize = require('./db');
 const cors = require('cors');
 const router = require('./routes/index');
+const fileUpload = require('express-fileupload');
 const errorHandler = require('./middleware/ErrorHandlingMiddleware');
 
 const PORT = process.env.PORT || 5000;
@@ -14,6 +15,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload({}));
 app.use('/api', router);
 
 //Error handler - is last middleware
