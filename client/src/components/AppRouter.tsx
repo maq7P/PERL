e784a86c4621e767fa201f404edbe707eee5e54e
maq7 <maxpet16@mail.ru>
2022-03-05 20:03:33 +0500

    [client]: have been created starting template

---
+++ client/src/components/AppRouter.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Routes, Route, Redirect} from 'react-router-dom'
+
+const AppRouter = () => {
+  return (
+    <Switch>
+
+    </Switch>
+  );
+};
+
+export default AppRouter;
