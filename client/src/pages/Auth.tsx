846a8042511c0f6c87741deb81d66ac807171b10
maq7 <maxpet16@mail.ru>
2022-03-07 16:02:47 +0500

    [client]: added layout for page Auth

--- client/src/pages/Auth.tsx
+++ client/src/pages/Auth.tsx
@@ -1,7 +1,68 @@
-import React from "react"
+import React, { useContext, useState } from "react"
+import { NavLink, useLocation } from "react-router-dom"
+
+import Container from "react-bootstrap/Container"
+import Card from "react-bootstrap/Card"
+import Form from "react-bootstrap/Form"
+import Row from "react-bootstrap/Row"
+import Button from "react-bootstrap/Button"
+
+import AppContext from "../context/AppContext"
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from "../utils/constants"
 
 const Auth = () => {
-  return <div>AUTH</div>
+  const { user } = useContext(AppContext)
+  const location = useLocation()
+
+  const [password, setPassword] = useState<string>("")
+  const [email, setEmail] = useState<string>("")
+
+  const isLogin = location.pathname === LOGIN_ROUTE
+
+  const handleClick = async () => {
+    console.log(email, password)
+  }
+
+  return (
+    <Container
+      className="d-flex justify-content-center align-items-center"
+      style={{ height: window.innerHeight - 54 }}
+    >
+      <Card style={{ width: 600 }} className="p-5">
+        <h2 className="m-auto">{isLogin ? "Авторизация" : "Регистрация"}</h2>
+        <Form className="d-flex flex-column">
+          <Form.Control
+            className="mt-3"
+            placeholder="Введите ваш email..."
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+          <Form.Control
+            className="mt-3"
+            placeholder="Введите ваш пароль..."
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            type="password"
+          />
+          <Row className="d-flex justify-content-between mt-3 pl-3 pr-3">
+            {isLogin ? (
+              <div>
+                Нет аккаунта?{" "}
+                <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйся!</NavLink>
+              </div>
+            ) : (
+              <div>
+                Есть аккаунт? <NavLink to={LOGIN_ROUTE}>Войдите!</NavLink>
+              </div>
+            )}
+            <Button variant={"outline-success"} onClick={handleClick}>
+              {isLogin ? "Войти" : "Регистрация"}
+            </Button>
+          </Row>
+        </Form>
+      </Card>
+    </Container>
+  )
 }
 
 export default Auth
